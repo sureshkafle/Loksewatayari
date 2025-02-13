cf2d563bb7dada251511f801d86a9d133911dda3
sureshkafle <sureshkafley@gmail.com>
2025-02-13 13:08:28 +0545

    create repository for category relations

--- QuizCore/Modules/Categories/CategoryRelationRepository.cs
+++ QuizCore/Modules/Categories/CategoryRelationRepository.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using QuizCore.Data.EF;
+using QuizCore.Modules.Categories.Entities;
 
 namespace QuizCore.Modules.Categories;
 
@@ -10,5 +12,27 @@ public class CategoryRelationRepository
      {
           _dbContext = dbContext;
      }
+
+     public async Task<int> Create (Guid categoryId, Guid[] subCategory)
+     {
+          foreach(var id in subCategory)
+          {
+               var entity = new CategoryRelation
+               {
+               Id=Guid.NewGuid(),
+               ParentCategoryId=categoryId,
+               ChildCategoryId=id,
+               };
+               _dbContext.CategoryRelations.Add(entity);
+          }
+          return await _dbContext.SaveChangesAsync();
+     }
+     public async Task<int> DeleteByCategory(Guid categoryId)
+     {
+          return await _dbContext.CategoryRelations
+          .Where(x=>x.ParentCategoryId==categoryId)
+          .ExecuteDeleteAsync();
+     }
+
      
 }
