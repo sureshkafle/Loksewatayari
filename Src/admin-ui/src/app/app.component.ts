27036c24fe5a758bdf3955de196ba8a6a33429e9
sureshkafle <sureshkafley@gmail.com>
2025-05-03 14:31:51 +0545

    added the header

--- Src/admin-ui/src/app/app.component.ts
+++ Src/admin-ui/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,  HomeComponent, HeaderComponent],
+  template: `<app-header /> 
+  <main>
+    <app-home />
+    </main>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
