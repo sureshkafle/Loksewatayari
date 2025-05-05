39dc6861cc58a015e9d02de16500c242f82b212a
sureshkafle <sureshkafley@gmail.com>
2025-05-05 08:37:41 +0545

    create user model

---
+++ Src/admin-ui/src/app/models/create-user.ts
@@ -0,0 +1,11 @@
+export interface createuser {
+     firstname:string,
+     middlename: string;
+     lastname:string;
+     username: string;
+     email: string;
+     password: string;
+     activestatus: boolean;
+     phonenumber: number;
+     dateofbirth: Date;
+}
