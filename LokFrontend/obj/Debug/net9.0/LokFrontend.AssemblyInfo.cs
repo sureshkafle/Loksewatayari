0b655de4ccedf1e0807bb5dc7baae74dd5c25687
sureshkafle <sureshkafley@gmail.com>
2025-11-08 20:28:02 +0545

    create notice detail

--- LokFrontend/obj/Debug/net9.0/LokFrontend.AssemblyInfo.cs
+++ LokFrontend/obj/Debug/net9.0/LokFrontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LokFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+566de5777c225fadc3510e0f561fcbf14f67f702")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac14f501c6fa6fb0c4a5c7458dacfa843fbb6a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LokFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LokFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
