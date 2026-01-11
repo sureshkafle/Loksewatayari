477a94a681b259acfae03989fb773d778b8d4485
sroshan-git <me.rroshan@gmail.com>
2026-01-11 22:44:07 +0545

    Added Practice Pages

---
+++ LokFrontend/Pages/QuizPage/Practicepage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LokFrontend.Pages.QuizPage
+{
+    public class PracticepageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
