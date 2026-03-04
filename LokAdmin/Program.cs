2d6784a7712b613ece11a58408b7d29a9a89ef56
sureshkafle <sureshkafley@gmail.com>
2026-03-04 09:04:33 +0545

    get api address from appsettings

--- LokAdmin/Program.cs
+++ LokAdmin/Program.cs
@@ -9,7 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["WebApiAddress"]) });
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthorizationCore(); 
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
